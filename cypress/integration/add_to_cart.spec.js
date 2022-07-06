b1a11995f3207e3446daabcffc0b8f26117800e1
ESDUNCAN <d.eric@rogers.com>
2022-07-06 14:55:03 +0000

    added Spec

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,14 @@
+
+
+describe("Add to Cart", () => {
+  it("should navigate to home", () => {
+    cy.visit("/");
+  })
+  it("should click on a product's add to cart", () => {
+    cy.contains("Scented Blade").parent('article').find(".btn").click({ force: true })
+
+  })
+  it("should increment by 1", () => {
+    cy.contains("My Cart (1)").should("exist")
+  })
+});
---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,17 @@
+
+
+describe("home", () => {
+  it("should navigate to home", () => {
+    cy.visit("/");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article")
+      .should("have.length", 2);
+  });
+  it("should click on a product", () => {
+    cy.contains("Scented Blade").click()
+  })
+  it("should show the product description", () => {
+    cy.contains("The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.").should("exist");
+  });
+});
